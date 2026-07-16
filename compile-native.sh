9567c7da3586196cf60bc5b54822e25baf49893c
Trix <cooldinothunder@gmail.com>
2026-07-15 23:28:52 -0400

    Pin RavePvP ext-pathfinder integration commit

--- compile-native.sh
+++ compile-native.sh
@@ -20,17 +20,18 @@ import sys
 path = Path(sys.argv[1])
 text = path.read_text()
 
+pathfinder_sha = "a575664430d2725f84689cb4a80dfa9f82218c85"
 math_sha = "ceee39ac0ece476accaa09e58aeb70f66276b6c7"
 nbt_sha = "099ccb67c57fc0893b2c388f89f363f89f389a51"
 
 anchor = 'EXT_PATHFINDER_VERSION="132f6c13d864bc7b48521555b9bd8cbd2706cb4c"\n'
-insert = anchor + f'EXT_MATH_VERSION="{math_sha}"\nEXT_NBT_VERSION="{nbt_sha}"\n'
+insert = f'EXT_PATHFINDER_VERSION="{pathfinder_sha}"\nEXT_MATH_VERSION="{math_sha}"\nEXT_NBT_VERSION="{nbt_sha}"\n'
 if anchor not in text:
     raise SystemExit("Unable to locate EXT_PATHFINDER_VERSION in compile.sh")
 text = text.replace(anchor, insert, 1)
 
 anchor = 'get_github_extension "pathfinder" "$EXT_PATHFINDER_VERSION" "TrixNEW" "ext-pathfinder"\n'
-insert = anchor + 'get_github_extension "ext_math" "$EXT_MATH_VERSION" "phpMine-MP" "ext-math"\nget_github_extension "ext_nbt" "$EXT_NBT_VERSION" "phpMine-MP" "ext-nbt"\n'
+insert = 'get_github_extension "pathfinder" "$EXT_PATHFINDER_VERSION" "RavePvP" "ext-pathfinder"\nget_github_extension "ext_math" "$EXT_MATH_VERSION" "phpMine-MP" "ext-math"\nget_github_extension "ext_nbt" "$EXT_NBT_VERSION" "phpMine-MP" "ext-nbt"\n'
 if anchor not in text:
     raise SystemExit("Unable to locate pathfinder extension download in compile.sh")
 text = text.replace(anchor, insert, 1)
